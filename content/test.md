66fee8a0cade098222efdf681231ca80404df61e
mattsykesj <mattsykesj@gmail.com>
2024-12-02 08:15:35 +0000

    added paragraph italic and bold support

--- content/test.md
+++ content/test.md
@@ -4,12 +4,14 @@ some normal text here
 
 **some bold text**
 
-*some italic text*
+*some italic text* last thing *italic*
 
-this is some normal text **bold here** but then an *italic* here
+this is some normal text **bold here** but then an *italic* here last thing **bold**
 
 ## A h2 header
 
 and some more
 
 ### A h3 header
+
+adding this for testing 
--- src/main.c
+++ src/main.c
@@ -16,6 +16,12 @@
 #define d64 double
 #define bool32 int32_t
 
+#define HTML_ITALIC_TAG_OPEN "<em>"
+#define HTML_ITALIC_TAG_CLOSE "</em>"
+#define HTML_BOLD_TAG_OPEN "<strong>"
+#define HTML_BOLD_TAG_CLOSE "</strong>"
+
+
 typedef enum
 {
     LineType_Paragraph,
@@ -72,32 +78,76 @@ static int parseHeader(char *line, u8 *buffer, size_t *currentPosition)
 static int parseParagraph(char *line, u8 *buffer, size_t *currentPosition)
 {
     u8 i = 0;
+    u32 htmlLinePosition = 0;
     stripNewLine(line);
 
     char htmlLine[1024] = {0};
+    bool32 inItalic = 0;
+    bool32 inBold = 0;
+
 
     while (line[i] != '\0')
     {
-        snprintf(htmlLine, sizeof(htmlLine), "%c", line[i]);
+
+        if(line[i] == '*')
+        {
+            if(line[i + 1] == '*')
+            {
+                //bold
+                if(inBold == 1)
+                {
+                    inBold = 0;
+                    snprintf(htmlLine + htmlLinePosition, sizeof(htmlLine), HTML_BOLD_TAG_CLOSE);
+                    htmlLinePosition += strlen(HTML_BOLD_TAG_CLOSE);
+                    i += 2; // skip the second *
+                    continue;
+                }
+
+                if(inBold == 0)
+                {
+                    inBold = 1;
+                    snprintf(htmlLine + htmlLinePosition, sizeof(htmlLine), HTML_BOLD_TAG_OPEN);
+                    htmlLinePosition += strlen(HTML_BOLD_TAG_OPEN);
+                    i += 2; // skip the second *
+                    continue;
+                } 
+
+            }
+            else
+            {
+                //italic
+                if(inItalic == 1)
+                {
+                    inItalic = 0;
+                    snprintf(htmlLine + htmlLinePosition, sizeof(htmlLine), HTML_ITALIC_TAG_CLOSE);
+                    htmlLinePosition += strlen(HTML_ITALIC_TAG_CLOSE);
+                    i++;
+                    continue;
+                }
+                
+                if(inItalic == 0)
+                {
+                    inItalic = 1;
+                    snprintf(htmlLine + htmlLinePosition, sizeof(htmlLine ), HTML_ITALIC_TAG_OPEN);
+                    htmlLinePosition += strlen(HTML_ITALIC_TAG_OPEN);
+                    i++;
+                    continue;
+                }
+                    
+            }
+        }
+
+        snprintf(htmlLine + htmlLinePosition, sizeof(htmlLine), "%c", line[i]);
         i++;
+        htmlLinePosition++;
     }
 
-    // snprintf(htmlLine, sizeof(htmlLine), "%s\n", line);
 
+    //process normal text
+    snprintf(htmlLine + htmlLinePosition, sizeof(htmlLine), "\n");
     size_t htmlLineContentLength = strlen(htmlLine);
-
     memcpy(buffer + *currentPosition, htmlLine, htmlLineContentLength);
     *currentPosition += htmlLineContentLength;
-
-    // // Skip '#' characters and the following space
-    // char *textStartPtr = line + headerCounter + 1;
-    // char htmlLine[1024] = {0};
-    // snprintf(htmlLine, sizeof(htmlLine), "<h%d>%s</h%d>\n", headerCounter, textStartPtr, headerCounter);
-
-    // size_t htmlLineContentLength = strlen(htmlLine);
-
-    // memcpy(buffer + *currentPosition, htmlLine, htmlLineContentLength);
-    // *currentPosition += htmlLineContentLength;
 
     return 0;
 }
@@ -167,74 +217,19 @@ int main(int argc, char *argv[])
             parseParagraph(line, buffer, &currentPosition);
             break;
         }
-
-        // while(line[star_counter] == '*')
-        // {
-        //     star_counter++;
-        // }
-
-        // //italic
-        // if(star_counter == 1)
-        // {
-        //     // Remove the **/n at the end of the string
-        //     size_t lineLength = strlen(line);
-        //     if (lineLength > 0 && line[lineLength - 1] == '\n' &&
-        //                            line[lineLength - 2] == '*')
-        //     {
-        //         line[lineLength - 2] = '\0';
-        //     }
-        //     else
-        //     {
-        //         //parsing error not valid italic line.
-        //     }
-
-        //     u8 *textStartPtr = line + star_counter;
-        //     u8 htmlLine[1024] = {0};
-        //     snprintf(htmlLine, sizeof(htmlLine), "<i>%s</i>\n", textStartPtr);
-
-        //     size_t htmlLineContentLength = strlen(htmlLine);
-
-        //     memcpy(buffer + currentPosition, htmlLine, htmlLineContentLength);
-        //     currentPosition += htmlLineContentLength;
-        //     continue;
-        // }
-
-        // //bold
-        // if(star_counter == 2)
-        // {
-        //     // Remove the **/n at the end of the string
-        //     size_t lineLength = strlen(line);
-        //     if (lineLength > 0 && line[lineLength - 1] == '\n' &&
-        //                            line[lineLength - 2] == '*' &&
-        //                            line[lineLength - 3] == '*')
-        //     {
-        //         line[lineLength - 3] = '\0';
-        //     }
-        //     else
-        //     {
-        //         //parsing error not valid bold line.
-        //     }
-
-        //     u8 *textStartPtr = line + star_counter;
-        //     u8 htmlLine[1024] = {0};
-        //     snprintf(htmlLine, sizeof(htmlLine), "<b>%s</b>\n", textStartPtr);
-
-        //     size_t htmlLineContentLength = strlen(htmlLine);
-
-        //     memcpy(buffer + currentPosition, htmlLine, htmlLineContentLength);
-        //     currentPosition += htmlLineContentLength;
-        //     continue;
-        // }
-
-        // headers
     }
 
     buffer[currentPosition] = '\0'; // Null terminate the buffer
+
+    FILE *htmlFile = fopen("output/test.html", "w");
+    fprintf(htmlFile, "%s", buffer);
+    fclose(htmlFile);
 
     printf("File contents:\n%s\n", buffer);
     printf("the file size is %ld!\n", fileSize);
 
     fclose(file);
+
     free(buffer);
 
     return 0;
